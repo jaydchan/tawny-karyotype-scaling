dc9554203df026fc8ad688e69409a95f16a298dc
Jennifer Warrender <jaydchan@gmail.com>
2014-03-27 17:12:41 +0000

    1. Total overhaul -- now automatic -- file creates 1 scatter plot (performace), 1 line chart (scaling results), and at most 4 (for now) bar charts (affects results).

--- src/ncl/karyotype/scaling/graph.clj
+++ src/ncl/karyotype/scaling/graph.clj
@@ -2,52 +2,116 @@
   (:use (incanter core stats charts))
   (:require [clojure.java.io :as io]))
 
-;; function to read a file
-(defn get-lines [file-name]
+(defn- get-lines [file-name]
+  "Reads in FILE_NAME."
   (with-open [r (io/reader file-name)]
     (doall (line-seq r))))
 
-;; reads in data
-(def string-results (get-lines "results.txt"))
-(def results
-  (for [r string-results]
-    (read-string r)))
+(defn- performance-plot [data]
+  "Produces (and saves) scatter plot showing performance results using
+given DATA."
+  (let [times (for [r data]
+                (get r 4))]
+    (def performance (scatter-plot
+                      (range 1 (count times))
+                      times
+                      :title "Performance check"
+                      :x-label "Run index"
+                      :y-label "Time taken (ms)")))
+  ;; (view performance)
+  (save performance "performance.png"))
 
-;; groups data by the key
-(def grouped-results (group-by first results))
+(defn- scaling-plot [data]
+  "Produces (and saves) line chart showing scaling results using given
+DATA."
+  (let [start (first data)]
+    (def scaling (line-chart
+               (map #(read-string (name %)) (keys (second start)))
+               (map #(log %) (vals (second start)))
+               ;; (vals (second start))
+               :legend true
+               :title "Scaling results"
+               :x-label "Number of random karyotypes (k)"
+               :y-label "Time taken (ms)"
+               :series-label (str "m = " (first start))))
+    (doseq [r (rest data)]
+      (add-categories scaling
+                      (map #(read-string (name %)) (keys (second r)))
+                      (map #(log %) (vals (second r)))
+                      ;; (vals (second start))
+                      :series-label (str "m = " (first r))))
+    ;; (view scaling)
+    ;; (Thread/sleep 1000) ;; needed???
+    (save scaling "scaling.png")))
 
-(def stuff
-  (for [r grouped-results]
-    (let [record (second r)]
-      [(first r)
-       (apply merge
-        (map
-         #(sorted-map (keyword (str (second %1))) (mean (second (rest %1))))
-         record))])))
+(import '(org.jfree.chart.renderer.category BarRenderer)
+        '(java.awt Color))
 
-(doseq [s stuff]
-  (println s))
+(def colors [Color/blue Color/red Color/green Color/yellow])
+(defn- affects-plot [data]
+  "Produces (and saves) bar charts showing affects results using given
+DATA."
+  (doseq [d data]
+    (let [chart (bar-chart
+                (map #(read-string (name %)) (keys (second d)))
+                (vals (second d))
+                :title (str (first d))
+                :x-label "Affects model (a)"
+                :y-label "Time taken (ms)")]
+      ;; custom-render class (and instance) such that bars are
+      ;; different colors
+      ;; http://www.java2s.com/Code/Java/Chart/JFreeChartBarChartDemo3differentcolorswithinaseries.htm
+      ;; http://stackoverflow.com/questions/1112709/when-you-extend-a-java-class-in-clojure-and-define-a-method-of-the-same-name-as
+      (def custom-render
+        (proxy [BarRenderer] []
+          (getItemPaint [row column]
+            (get colors (mod column (count colors))))))
+      (.setRenderer (.getPlot chart) custom-render)
+      ;; (view chart)
+      (save chart (str "affects" (first d) ".png")))))
 
-(let [start (first stuff)]
-  (def plot (line-chart
-             (map #(read-string (name %)) (keys (second start)))
-             (map #(log %) (vals (second start)))
-             ;; (vals (second start))
-             :legend true
-             :title "Line chart showing the average time taken (n =
-             100 iterations) versus the number of random
-             karyotypes (k) with at most m number of restrictions."
-             :x-label "Number of random karyotypes (k)"
-             :y-label "Time taken (msecs)"
-             :series-label (str "m = " (first start))))
-  (doseq [r (rest stuff)]
-    (add-categories plot
-                    (map #(read-string (name %)) (keys (second r)))
-                    (map #(log %) (vals (second r)))
-                    ;; (vals (second r))
-                    :series-label (str "m = " (first r))))
-  (view plot)
-  (Thread/sleep 10000)
-  (save plot "log-linear.png")
-  ;; (save plot "linear-linear.png")
-)
+(defn driver [avalue mvalue]
+  ;; reads in data
+  (def string-results (get-lines "results.txt"))
+  (def results
+    (for [r string-results]
+      (read-string r)))
+
+  ;; PERFORMANCE CHECK
+  (performance-plot results)
+
+  ;; SCALING RESULTS
+  (let [a-groups (group-by first results)
+        affects (get a-groups avalue)
+        m-groups (group-by #(get % 2) affects)]
+
+    (def scaling
+      (for [m m-groups]
+        (let [records (second m)
+              k-groups (group-by #(get % 1) records)]
+          [(first m)
+           (apply merge
+                  (map
+                   #(sorted-map (keyword (str (first %)))
+                                (mean (for [record (second %)]
+                                        (get record 4))))
+                   k-groups))]))))
+  (scaling-plot scaling)
+
+  ;; AFFECTS RESULTS
+  (let [m-groups (group-by #(get % 2) results)
+        max (get m-groups mvalue)
+        k-groups (group-by #(get % 1) max)]
+
+    (def affects
+      (for [k k-groups]
+        (let [records (second k)
+              a-groups (group-by #(get % 0) records)]
+          [(first k)
+           (apply merge
+                  (map
+                   #(sorted-map (keyword (str (first %)))
+                                (mean (for [record (second %)]
+                                        (get record 4))))
+                   a-groups))]))))
+  (affects-plot affects))
