0658c8844278e84eafa5f5a4e8c2a9c9fbe373ff
Jennifer Warrender <jaydchan@gmail.com>
2014-03-27 16:57:43 +0000

    1. Automation now implemented. 2. Generation of base ontologies, refining using affects models, reasoning of all ontologies, and graph generation implemented. 3. randomisation of tasks implmented.

--- driver.sh
+++ driver.sh
@@ -1,48 +1,77 @@
 #!/bin/bash
 
-M=( 1 3 6 10) 
-K=( 10 100 1000 10000 )
+AFFECTS=("without" "affects1" "affects2" "affects3" )
+# A=( `seq 0 $((${#AFFECTS[@]}-1))` )
+A=( $(eval echo {0..$((${#AFFECTS[@]}-1))} ))
 
-## generate owl ontologies
-# for m in "${M[@]}"
-# do
-#     echo $m > m.txt
-#     for k in "${K[@]}"
-#     do
-# 	echo $k > k.txt
-# 	for n in {1..100}
-# 	do
-# 	    echo "Generating m=$m k=$k n=$n"
-# 	    echo $n > n.txt
-# 	    lein run
-# 	    mv "n$n.owl" "resources/m$m/k$k"
-# 	done
-#     done
-# done
+K=( 10 100 1000 10000 ) # 100000
 
-#clean-up
-echo "" > results.txt
+# M=( `seq 0 10` )
+# M=( $(eval echo {0..10} ))
+M=( 0 5 )
 
-## scaling results
-for m in "${M[@]}"
-do
-    echo $m > m.txt
-    for k in "${K[@]}"
-    do
-	echo $k > k.txt
-	echo "[$m $k " >> results.txt
-	for n in {1..100}
-	do
-	    echo "Reasoning m=$m k=$k n=$n"
-	    echo $n > n.txt
-	    cp "resources/m$m/k$k/n$n.owl" ./resources
-	    lein run >> results.txt
-	    rm "resources/n$n.owl"
+# N=( `seq 1 100` )
+# N=( $(eval echo {1..100} ))
+N=( 1 2 )
+
+## run driver? YES=1 NO=0
+GEN=0; ## generate base ontologies
+REF=0; ## refine ontologies with affects models
+REA=0; ## reason ontologies and note time
+
+## clean-up
+echo -n > tasks.txt
+
+## generate and refine ontologies
+for a in "${A[@]}"; do
+    for k in "${K[@]}"; do
+	for m in "${M[@]}"; do
+	    for n in "${N[@]}"; do
+		if [ $a = 0 -a $GEN = 1 ]; then
+		    # if a == 0, then generate ontology
+		    echo "Generating a=$a k=$k m=$m n=$n"
+		    lein run $a $k $m $n
+		    mv "n$n.owl" "resources/without/k$k/m$m"
+		elif [ $a -ge 1 -a $a -le 3 -a $REF = 1 ]; then
+		    # else if (1 <= a >= 3) then refine ontology
+		    echo "Generating a=$a k=$k m=$m n=$n"
+		    cp "resources/without/k$k/m$m/n$n.owl" ./resources
+		    lein run $a $k $m $n
+		    mv "n$n.owl" "resources/affects$a/k$k/m$m/n$n.owl"
+		    rm "resources/n$n.owl"
+		fi
+		echo "$a $k $m $n" >> tasks.txt ## note tasks
+	    done
 	done
     done
 done
 
-#clean-up
-rm n.txt
-rm k.txt
-rm m.txt
+## randomise the tasks
+shuf tasks.txt -o tasks.txt
+
+## reason ontologies
+readarray LINES < tasks.txt
+
+if [ $REA = 1 ]; then
+    ## clean up
+    echo -n > results.txt
+
+    for line in "${LINES[@]}"; do
+	PARTS=( $line )
+	echo "Reasoning a=${PARTS[0]} k=${PARTS[1]} m=${PARTS[2]} n=${PARTS[3]}"
+	cp "resources/${AFFECTS[${PARTS[0]}]}/k${PARTS[1]}/m${PARTS[2]}/n${PARTS[3]}.owl" ./resources
+	echo -n "[${PARTS[0]} ${PARTS[1]} ${PARTS[2]} ${PARTS[3]} " >> results.txt
+	lein run -1 ${PARTS[1]} ${PARTS[2]} ${PARTS[3]} >> results.txt
+	echo "]" >> results.txt
+	rm resources/n${PARTS[3]}.owl
+    done
+    echo "Reasoning complete"
+fi
+
+## clean up
+rm tasks.txt
+
+## graphs
+echo "Creating graphs"
+lein run -2
+mv *.png ./resources/graphs
