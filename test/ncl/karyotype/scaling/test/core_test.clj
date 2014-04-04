f59b278541580436728f9d2def4dd5465c62a4d2
Jennifer Warrender <jaydchan@gmail.com>
2014-04-04 03:41:17 +0100

    initial checkin

---
+++ test/ncl/karyotype/scaling/test/core_test.clj
@@ -0,0 +1,8 @@
+(ns ncl.karyotype.scaling.test.core_test
+  (:use [ncl.karyotype.scaling.core]
+        [clojure.test]
+        [tawny.owl]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
